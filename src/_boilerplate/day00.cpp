df8a7a56571a90afda71874a5bfcb866b2453eb7
Fabrício Milanez <fabriciomilanez@protonmail.com>
2022-12-04 21:18:54 -0300

    Updated boilerplate code.
    Each day's solution program now automatically determines what part it
    should solve the input for by checking the presence of a file that
    contains the answer for the first part.

--- src/_boilerplate/day00.cpp
+++ src/_boilerplate/day00.cpp
@@ -1,7 +1,14 @@
+#include <filesystem>
 #include <fstream>
 
 #include "fmt/format.h"
 #include "fmt/ranges.h"
+
+void part1(std::ifstream& input) {
+}
+
+void part2(std::ifstream& input) {
+}
 
 /**
  * https://adventofcode.com/2022/day/
@@ -9,11 +16,11 @@
  * Usage: ./day00 [input-file]
 */
 int main(const int argc, const char** argv) {
+    namespace fs = std::filesystem;
+
     const auto filename = argc <= 1 ? "./input.txt" : argv[1];
     std::ifstream input{filename};
 
-    for (std::string moves; getline(input, moves); ) {
-        // ...
-    }
-    // fmt::print("{}\n", answer);
+    const auto solve = (not fs::exists("./pt1.answer")) ? &part1 : &part2;
+    solve(input);
 }
