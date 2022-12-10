fc77f0d282860cbdd4ac75d481c86c8083241022
fabberr <fabriciomilanez@protonmail.com>
2022-12-10 12:37:24 -0300

    Updated boilerplate code.

--- src/_boilerplate/day00.cpp
+++ src/_boilerplate/day00.cpp
@@ -1,13 +1,20 @@
 #include <filesystem>
+namespace fs = std::filesystem;
+
 #include <fstream>
+#include <string_view>
 
 #include "fmt/format.h"
 #include "fmt/ranges.h"
 
+/** Solution for part one of the puzzle. */
 void part1(std::ifstream& input) {
+    input.close();
 }
 
+/** Solution for part two of the puzzle. */
 void part2(std::ifstream& input) {
+    input.close();
 }
 
 /**
@@ -16,7 +23,6 @@ void part2(std::ifstream& input) {
  * Usage: ./day00 [input-file]
 */
 int main(const int argc, const char** argv) {
-    namespace fs = std::filesystem;
 
     const auto filename = argc <= 1 ? "./input.txt" : argv[1];
     std::ifstream input{filename};
